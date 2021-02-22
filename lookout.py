cf7013f0df83a4fb89db996946bda80de0cbc98c
darrellmiller <darrellrhodesmiller@gmail.com>
2021-02-22 14:57:02 -0600

    Initial commit

---
+++ lookout.py
@@ -0,0 +1,16 @@
+# Louisiana State Police / Louisiana Cyber Investigators Alliance
+# Project Lookout Query Tool: takes either a file or folder, pulls out all public IP addresses and queries multiple
+#   sources for threat information. Then builds a report
+# - Resources:
+#   -- www.la-safe.org : Louisiana State Police Fusion Center (our point of contect information)
+#   -- shodan.io : Search Engine of Internet of Things Devices
+#   -- lookingglasscyber.com : scoutPrime threat database
+#   -- csirtg.io : CIFv5 open source cyber threat database
+#   -- Abeebus: https://github.com/13Cubed/Abeebus : scrapes external ips from text file, geo locates them
+
+# Dependencies:
+
+
+# Main
+if __name__ == '__main__':
+    print ("main stuff will go here")
