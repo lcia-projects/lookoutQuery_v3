5de6c674d3c01ccd6634350c1c1f9b1d0c7d62e0
darrellmiller <darrellrhodesmiller@gmail.com>
2021-02-26 10:10:55 -0600

    Basic argument parsing put in place and reading of input files in place

--- lookout.py
+++ lookout.py
@@ -10,9 +10,67 @@
 
 # Dependencies:
 import yaml
+import argparse
+from os import path
+from glob import glob
+
 
 from Resources import libLookout
 
+# Allows user to modify paremeters from commandline at runtime
+def argsParse():
+    # ArgParse Configuration
+    parser = argparse.ArgumentParser(description='Lookout Query v3',
+                                     usage='main.py <folder> [-w outfile] ', add_help=False)
+    parser.add_argument('--folder', help='folder path, process files within folder', nargs="*")
+    parser.add_argument('--filename', help='single file to process')
+    parser.add_argument('--cif', action="store_true",
+                        help='query Cyber Intelligence Framework (internal self-hosted server)', required=False)
+    parser.add_argument('--firehol', action="store_true", help='query FireHOL blocklists', required=False)
+    parser.add_argument('--scoutprime', action="store_true", help='query LookingGlass Scout Prime', required=False)
+    parser.add_argument('--shodan', action="store_true", help='Query Shodan (very slow)', required=False)
+    parser.add_argument('--all', action="store_true", help='Query All Available Modules', required=False)
+    parser.add_argument('--fullreport', action="store_true", help='Full Indepth Report (MUCH longer)', required=False)
+    parser.add_argument('--output', help='set an output folder, if blank, folder will be date-time', required=False)
+    parser.add_argument('-h', '--help', action='help', default=argparse.SUPPRESS,
+                        help='Show this help message and exit')
+    args = vars(parser.parse_args())
+
+    return args
+
 # Main
 if __name__ == '__main__':
+    YAMLFILE="./lookout.yaml"
+
+    if (path.exists(YAMLFILE)):
+        print("Loading yaml settings file")
+        # -- Loads Configuration File for LookOut --
+        with open(YAMLFILE,"r") as file:
+            lookout_config = yaml.load(file, Loader=yaml.FullLoader)
+    else:
+        print("ERROR: No config file, please refer to lookout.yml.example in root folder of script")
+        exit()
+
+    # Process and error check commandline arguments
+    args=argsParse()
+
+    # if no folder or file given, defaults to what is in the lookout.yaml configuration file
+    if args['folder']==None and args['filename']==None:
+        print (" --: No Folder or Filename given for processing, using default value in Lookout.yaml <input folder>")
+        args['folder']=lookout_config['lookout_default_inputFolder']
+
+    # Read in File(s)
+    if args['folder']:
+        folderName = ((args['folder']) + "/*")
+        fileList = glob(folderName)
+    elif args['filename']:
+        fileList = []
+        fileList.append(args['filename'])
+        # print (fileList)
+    else:
+        print("ERROR:something went wrong, you cant have a folder and a file selection.")
+        exit()
+
+    print (" --: Folder:", folderName)
+    print (" --: FileList to process:", fileList)
     lookoutObj=libLookout.lookout()
